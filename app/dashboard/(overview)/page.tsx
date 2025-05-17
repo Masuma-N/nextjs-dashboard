57f48add7bb626441baff3da2e756a5537bf4213
Masuma-N <113317957+Masuma-N@users.noreply.github.com>
2025-05-17 02:22:03 -0400

    temp dis overview 3

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,10 @@
+export default function OverviewFallbackPage() {
+  return (
+    <div className="p-6 text-xl">
+      Redirect fallback hit: You’re logged in, but this page is disabled.
+    </div>
+  );
+} 
+ 
+
+
