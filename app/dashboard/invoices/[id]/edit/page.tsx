858d98c8199951c29c842810ace301de79ad5cd0
Masuma-N <113317957+Masuma-N@users.noreply.github.com>
2025-05-16 23:35:11 -0400

    i try again

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,13 +3,11 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function Page({ params }: PageProps) {
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
   const id = params.id;
 
   const [invoice, customers] = await Promise.all([
@@ -37,6 +35,7 @@ export default async function Page({ params }: PageProps) {
     </main>
   );
 } 
+ 
 
 
 
