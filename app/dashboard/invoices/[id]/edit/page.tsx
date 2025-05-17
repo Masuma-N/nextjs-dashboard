9da4994c1d0db3f6ab0259ee1e13df32dcedd730
Masuma-N <113317957+Masuma-N@users.noreply.github.com>
2025-05-16 23:54:29 -0400

    11

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -8,6 +8,10 @@ import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
 export default async function Page({
   searchParams,
 }: {
@@ -37,8 +41,8 @@ export default async function Page({
         <Pagination totalPages={totalPages} />
       </div>
     </div>
-  ); 
-}
+  );
+} 
 
 
 
