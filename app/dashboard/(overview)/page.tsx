9681d5c1b763a633322ca830338786de01afdd6d
Masuma-N <113317957+Masuma-N@users.noreply.github.com>
2025-05-17 02:46:43 -0400

    fix dynamic import crash

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,6 @@
 import { Suspense } from 'react';
-import RevenueChartWrapper from '../revenue-wrapper'; // correct import!
-import LatestInvoices from '../latest-invoices'; 
-
-
+import RevenueChart from '../revenue-chart';
+import LatestInvoices from '../latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
@@ -10,7 +8,9 @@ export default function Page() {
     <main className="flex w-full flex-col gap-6">
       <h1 className={`${lusitana.className} text-2xl`}>Dashboard</h1>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-4">
-        <RevenueChartWrapper />
+        <Suspense fallback={<div>Loading chart...</div>}>
+          <RevenueChart />
+        </Suspense>
         <Suspense fallback={<div>Loading invoices...</div>}>
           <LatestInvoices />
         </Suspense>
@@ -18,6 +18,7 @@ export default function Page() {
     </main>
   );
 } 
+
 
  
 
